75fe6250fe5843322ae37c996f5f4f1379c01416
SimationStudios <brian@simationstudios.com>
2024-01-29 18:07:14 +0500

    create intro screen

--- src/services/utilities/ApiInstance/index.js
+++ src/services/utilities/ApiInstance/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const baseURL = 'http://192.168.100.59:5000/'
-// const baseURL = "https://exhaust-backend.netlify.app/.netlify/functions/api/"
-const baseURL = "http://192.168.100.59:9999/.netlify/functions/api/"
+const baseURL = "https://exhaust-backend.netlify.app/.netlify/functions/api/"
+// const baseURL = "http://192.168.100.59:9999/.netlify/functions/api/"
 const timeout = 5000
 
 const apiInstance = axios.create({
