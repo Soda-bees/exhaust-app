7a248120320e866b34e18be65aa3e507e61b677a
SimationStudios <brian@simationstudios.com>
2024-03-05 12:50:36 +0500

    change url

--- src/services/utilities/ApiInstance/index.js
+++ src/services/utilities/ApiInstance/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-export const baseURL = 'http://192.168.100.59:5000/'
-// export const baseURL = "https://exhaust-backend.netlify.app/.netlify/functions/api/"
-// export const baseURL = "http://192.168.100.59:9999/.netlify/functions/api/"
-const timeout = 5000
+// export const baseURL = 'http://192.168.100.59:5000/'
+export const baseURL = 'https://exhaust-6ea7a8c16534.herokuapp.com/'
+
+const timeout = 10000
 
 const apiInstance = axios.create({
     baseURL,
